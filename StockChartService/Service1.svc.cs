5b3a9e8faafe40b811d4b8ce94abeeb146615b5b
John Robertson <jj.robertson@asu.edu>
2014-11-07 21:07:52 -0800

    StockChartService now creates a chart, but the datapoints are backward.  Also, the remote IIS server is inaccessible, so the chart service only calls the local DataPointsService.  Tomorrow, I will make sure that everything is working.

---
+++ StockChartService/Service1.svc.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using StockChartService.ChartMakerService;
+using System.Runtime.Caching;
+using StockChartService.StockDataPoints;
+namespace StockChartService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+
+        //global cache object
+        ObjectCache cache = MemoryCache.Default;
+
+        public string getStockChartURL(string companyName)
+        {
+            string stockTicker = getStockTickerForCompanyName(companyName);
+
+            if (stockTicker == null)
+            {
+                return null;
+            }
+
+            if (cache.Contains(companyName))
+            {
+                return cache[companyName] as string;
+            }
+
+            string apiCall = getChartAPICall(stockTicker);
+
+            cache[companyName] = apiCall;
+
+            return apiCall;
+        }
+
+        private string getStockTickerForCompanyName(string companyName)
+        {
+            //returns stock ticker if company name is valid, else it returns null
+            return companyName;
+        }
+
+        private string getChartAPICall(string stockTicker){
+            //create service proxies
+            StockDataPointsServiceClient stockDataClient = new StockDataPointsServiceClient();
+            StockDataPoints.StockDataPoints pointsToPlot = stockDataClient.GetStockDataPoints(stockTicker);
+            
+            StringBuilder result = new StringBuilder();
+            result.Append("http://www.gxchart.com/service/Drawchart.aspx?type=LINE&Categories=Values:");
+
+            //append x values (dates) to the url
+            for(int i = 0; i < pointsToPlot.DataPoints.Length; i++){
+                if(pointsToPlot.DataPoints[i] == null)
+                    break;
+                if(i != 0)
+                    result.Append(",");
+
+                result.Append(pointsToPlot.DataPoints[i].date.ToShortDateString());
+            }
+
+            result.Append("&Series1=Values:stock price:");
+            //append y values (prices) to the url
+            for(int i = 0; i < pointsToPlot.DataPoints.Length; i++){
+                if(pointsToPlot.DataPoints[i] == null)
+                    break;
+                if(i != 0)
+                    result.Append(",");
+                result.Append(pointsToPlot.DataPoints[i].price);
+            }
+
+
+            result.Append("&title=Stock Price&width=500&height=310&domtitle=Date&rantitle=Amounts in USD&bgc1=88,125,88&bgc2=243,243,243&gbgc1=240,255,210&gbgc2=50,50,80&shadow=1&border=1&values=1&legend=left");
+
+            return result.ToString();
+        }
+    }
+}
